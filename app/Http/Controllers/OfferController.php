f739962296654e159416d7e17fb024498d651080
root <root@PS-SOHO075-01100.localdomain>
2025-09-19 16:58:24 -0300

    controller com routes no annotation

---
+++ app/Http/Controllers/OfferController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Offer;
+use Illuminate\Routing\Attributes\Get;
+use Illuminate\Routing\Attributes\Post;
+
+class OfferController extends Controller
+{
+    #[Get('/api/offers')]
+    public function index()
+    {
+        return response()->json(Offer::all());
+    }
+
+    #[Post('/api/offers')]
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'currency' => 'required|string|max:3',
+            'status' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+        ]);
+
+        $offer = Offer::create($validated);
+
+        return response()->json($offer, 201);
+    }
+}
--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -7,6 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
 {
-    /** @use HasFactory<\Database\Factories\OfferFactory> */
     use HasFactory;
+
+    protected $table = 'offers';
+
+    // Campos que podem ser preenchidos em massa
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'currency',
+        'status',
+        'start_date',
+        'end_date',
+    ];
+
+    // Se quiser tratar datas como objetos Carbon
+    protected $dates = [
+        'start_date',
+        'end_date',
+    ];
 }
