478848355b633010cc17ea29837a6f85586020ce
root <root@PS-SOHO075-01100.localdomain>
2025-09-26 08:58:15 -0300

    teste

---
+++ app/Http/Controllers/EnterpriseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreenterpriseRequest;
+use App\Http\Requests\UpdateenterpriseRequest;
+use App\Models\enterprise;
+
+class EnterpriseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreenterpriseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(enterprise $enterprise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(enterprise $enterprise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateenterpriseRequest $request, enterprise $enterprise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(enterprise $enterprise)
+    {
+        //
+    }
+}
---
+++ app/Models/enterprise.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class enterprise extends Model
+{
+    /** @use HasFactory<\Database\Factories\EnterpriseFactory> */
+    use HasFactory;
+}
