edb370731c7ddc67d5e7dc3b406cefb10390a713
root <root@PS-SOHO075-01100.localdomain>
2025-09-26 10:13:19 -0300

    novo repo de enterprises

---
+++ app/Repositories/EnterpriseRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Enterprise;
+use Illuminate\Database\Eloquent\Collection;
+
+interface EnterpriseRepositoryInterface
+{
+    public function list(): Collection;
+
+    /**
+     * @param  array<string, mixed>  $attributes
+     */
+    public function create(array $attributes): Enterprise;
+
+    /**
+     * @param  array<string, mixed>  $attributes
+     */
+    public function update(Enterprise $enterprise, array $attributes): Enterprise;
+
+    public function delete(Enterprise $enterprise): void;
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\EnterpriseController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\OfferController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->group(function () {
+    Route::get('enterprises', [EnterpriseController::class, 'index']);
+    Route::post('enterprises', [EnterpriseController::class, 'store']);
+    Route::delete('enterprises/{enterprise}', [EnterpriseController::class, 'destroy']);
+
     Route::get('categories', [CategoryController::class, 'index']);
     Route::post('categories', [CategoryController::class, 'store']);
     Route::put('categories/{category}', [CategoryController::class, 'update']);
